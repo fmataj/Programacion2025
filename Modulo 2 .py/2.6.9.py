56f3533f63ea6a42db2161c32d2751a53602afe6
fmataj <fmataj@ctpplatanares.ed.cr>
2025-05-30 15:51:58 -0600

    NOOTEBOOK 2. Cotidiano 30/5/2025

---
+++ Modulo 2 .py/2.6.9.py
@@ -0,0 +1,10 @@
+num1 = input("Ingrese el primer numero")
+num2 = input("Ingrese el primer numero")
+
+
+print(num1 + num2 )
+print(num1 - num2 )
+print(num1 * num2 )
+print(num1 / num2 )
+
+print("\n¡Eso es todo, amigos!")
