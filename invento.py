3f7e59ce3226de8d9ac968b3a62a2b766f1f2ca3
fmataj <fmataj@ctpplatanares.ed.cr>
2025-08-01 13:23:58 -0600

    Notebook 4
    aaa

---
+++ invento.py
@@ -0,0 +1,23 @@
+import pywhatkit as kit
+from datetime import datetime
+
+def enviar_whatsapp(numero: str,
+                    mensaje: str,
+                    hora: int,
+                    minuto: int):
+    
+    kit.sendwhatmsg(
+        "+{numero}",
+        mensaje,
+        hora,
+        minuto,
+        wait_time=20,
+    )
+    
+if __name__== "__main__":
+    numero = "50685179043"
+    mensaje = "Despierta profe ya lo hice"
+    hora = 13
+    minuto = 30
+    
+    enviar_whatsapp(numero,mensaje,hora,minuto)
